e0020672344d6b91d85e86cd40c45884dc3b37f3
Matt <taylor607@marshall.edu>
2015-12-02 01:54:48 -0500

    yay git commit -m yay

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,8 @@
 class Student < ActiveRecord::Base
 	attr_accessor :new_password, :new_password_confirmation
 
+  validates :firstname, presence: true
+  validates :lastname, presence: true
   validates :sid, presence: true, uniqueness: true
   validates :new_password, presence: true, length: { in: 6..20 }
   validates :new_password_confirmation, presence: true
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,8 @@
 class Teacher < ActiveRecord::Base
 	attr_accessor :new_password, :new_password_confirmation, :super_box
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
   validates :email, uniqueness: true
   validates :new_password, presence: true, length: { in: 6..20 }
   validates :new_password_confirmation, presence: true
